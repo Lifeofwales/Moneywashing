73660527f09f7fd8b37eb60ff6b9b5d8f7f98364
Lifeofwales <101365716+Lifeofwales@users.noreply.github.com>
2026-07-31 18:07:56 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -206,8 +206,6 @@ async function saveGang(event) {
     editId ? "Gang updated." : "Gang added."
   );
   resetGangForm();
-  resetUserForm();
-  startUsersListener();
 }
 
 async function deleteGang(id) {
